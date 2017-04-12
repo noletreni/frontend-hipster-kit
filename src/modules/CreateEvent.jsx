6d4670212e399a75ae439e5b67e59ed01213d1df
Oleg Stikhin <stikhio1@niksula.hut.fi>
2017-04-12 12:14:20 +0300

    Implement event page

--- src/modules/CreateEvent.jsx
+++ src/modules/CreateEvent.jsx
@@ -22,17 +22,44 @@ class CreateEvent extends React.Component {
     refresh();
   }
 
+  constructor() {
+    super();
+
+    const {
+      startTime,
+      endTime,
+    } = this.state;
+
+    startTime.setMinutes(0);
+    startTime.setHours(0);
+
+    endTime.setMinutes(59);
+    endTime.setHours(23);
+  }
+
   state = {
     name: '',
-    startDate: null,
-    startTime: null,
-    endDate: null,
-    endTime: null,
+    startDate: new Date(),
+    startTime: new Date(),
+    endDate: new Date(),
+    endTime: new Date(),
     description: '',
   };
 
   render() {
-    const { locale, intl: { formatMessage } } = this.props;
+    const {
+      create,
+      error,
+      locale,
+      intl: { formatMessage },
+    } = this.props;
+
+    const {
+      startDate,
+      startTime,
+      endDate,
+      endTime,
+    } = this.state;
 
     return (
       <CardWrapper>
@@ -46,23 +73,27 @@ class CreateEvent extends React.Component {
             />
             <DatePicker
               hintText={formatMessage({ id: 'eventStartDate' })}
+              value={startDate}
               onChange={(event, startDate) => this.setState({ startDate })}
               locale={locale}
             />
             <TimePicker
               format="24hr"
               hintText={formatMessage({ id: 'eventStartTime' })}
+              value={startTime}
               onChange={(event, startTime) => this.setState({ startTime })}
               locale={locale}
             />
             <DatePicker
               hintText={formatMessage({ id: 'eventEndDate' })}
+              value={endDate}
               onChange={(event, endDate) => this.setState({ endDate })}
               locale={locale}
             />
             <TimePicker
               format="24hr"
               hintText={formatMessage({ id: 'eventEndTime' })}
+              value={endTime}
               onChange={(event, endTime) => this.setState({ endTime })}
               locale={locale}
             />
@@ -72,6 +103,16 @@ class CreateEvent extends React.Component {
               value={this.state.description}
               onChange={event => this.setState({ description: event.target.value })}
             />
+
+            <Text>{ error }</Text>
+
+            <Button
+              raised
+              primary
+              onTouchTap={() => create(this.state)}
+            >
+              {formatMessage({ id: 'send' })}
+            </Button>
           </CardContent>
 
         </ResponsiveCard>
@@ -92,11 +133,32 @@ CreateEvent.propTypes = {
 export default injectIntl(connect(
   state => ({
     events: state.events.data,
+    error: state.events.error,
     locale: state.intl.locale,
   }),
   dispatch => ({
     refresh: () => {
       dispatch(rest.actions.events());
+    },
+    create: (event) => {
+      const timedEvent = event;
+
+      timedEvent.startDate.setHours(event.startTime.getHours());
+      timedEvent.startDate.setMinutes(event.startTime.getMinutes());
+      timedEvent.startDate.setSeconds(0);
+      timedEvent.startDate.setMilliseconds(0);
+
+      timedEvent.endDate.setHours(event.endTime.getHours());
+      timedEvent.endDate.setMinutes(event.endTime.getMinutes());
+      timedEvent.endDate.setSeconds(0);
+      timedEvent.endDate.setMilliseconds(0);
+
+      delete timedEvent.startTime;
+      delete timedEvent.endTime;
+
+      dispatch(rest.actions.events.post({}, {
+        body: JSON.stringify(timedEvent),
+      }));
     },
   }),
 )(CreateEvent));
---
+++ src/modules/EventPage.jsx
@@ -0,0 +1,76 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { injectIntl } from 'react-intl';
+
+import { push } from 'connected-react-router';
+
+import {
+  CardContent,
+} from 'material-ui/Card';
+import Text from 'material-ui/Text';
+
+import rest from '../utils/rest';
+
+import CardWrapper from '../components/CardWrapper';
+import ResponsiveCard from '../components/ResponsiveCard';
+
+class EventPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dialogOpen: false,
+      id: 3, //TODO: replace with actual id
+    };
+  }
+
+
+  componentDidMount() {
+    const { refreshEvent } = this.props;
+
+    refreshEvent(this.state.id);
+  }
+
+  render() {
+    const {
+      eventDetails,
+      locale,
+      intl: { formatMessage },
+     } = this.props;
+
+    const dateOptions = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+    }
+    return (
+      <CardWrapper>
+        <ResponsiveCard>
+          <CardContent>
+            <Text type="headline" component="h1">{ eventDetails.data.name }</Text>
+            <Text type="caption">{ formatMessage({ id: 'createdBy' })} { eventDetails.data.ownerName }</Text>
+            <Text type="body1"><b>{ formatMessage({ id: 'eventStartTime' })}:</b> { new Date(eventDetails.data.startDate).toLocaleString(locale, dateOptions) }</Text>
+            <Text type="body1"><b>{ formatMessage({ id: 'description' })}:</b> { eventDetails.data.description }</Text>
+
+          </CardContent>
+        </ResponsiveCard>
+      </CardWrapper>
+    );
+  }
+}
+
+export default injectIntl(connect(
+  state => ({
+    events: state.events,
+    eventDetails: state.eventDetails,
+    locale: state.intl.locale,
+  }),
+  dispatch => ({
+    refreshEvent: (eventId) => {
+      dispatch(rest.actions.eventDetails({ eventId }));
+    },
+  }),
+)(EventPage));
