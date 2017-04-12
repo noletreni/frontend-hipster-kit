6be065545b27180377732d97c1c3b4e454f12f49
Rasmus Eskola <fruitiex@gmail.com>
2017-04-12 12:59:39 +0300

    Creating events works

--- src/modules/CreateEvent.jsx
+++ src/modules/CreateEvent.jsx
@@ -22,17 +22,48 @@ class CreateEvent extends React.Component {
     refresh();
   }
 
+  constructor() {
+    super();
+
+    const {
+      startTime,
+      endTime,
+    } = this.state;
+
+    startTime.setMilliseconds(0);
+    startTime.setSeconds(0);
+    startTime.setMinutes(0);
+    startTime.setHours(0);
+
+    endTime.setMilliseconds(0);
+    endTime.setSeconds(0);
+    endTime.setMinutes(59);
+    endTime.setHours(23);
+  }
+
   state = {
     name: '',
-    startDate: null,
-    startTime: null,
-    endDate: null,
-    endTime: null,
+    startDate: new Date(),
+    startTime: new Date(),
+    endDate: new Date(),
+    endTime: new Date(),
     description: '',
   };
 
   render() {
-    const { locale, intl: { formatMessage } } = this.props;
+    const {
+      create,
+      error,
+      locale,
+      intl: { formatMessage },
+    } = this.props;
+
+    const {
+      startDate,
+      startTime,
+      endDate,
+      endTime,
+    } = this.state;
 
     return (
       <CardWrapper>
@@ -46,23 +77,27 @@ class CreateEvent extends React.Component {
             />
             <DatePicker
               hintText={formatMessage({ id: 'eventStartDate' })}
+              value={startDate}
               onChange={(event, startDate) => this.setState({ startDate })}
               locale={locale}
             />
             <TimePicker
               format="24hr"
               hintText={formatMessage({ id: 'eventStartTime' })}
+              value={startTime}
               onChange={(event, startTime) => this.setState({ startTime })}
               locale={locale}
             />
             <DatePicker
               hintText={formatMessage({ id: 'eventEndDate' })}
+              value={endDate}
               onChange={(event, endDate) => this.setState({ endDate })}
               locale={locale}
             />
             <TimePicker
               format="24hr"
               hintText={formatMessage({ id: 'eventEndTime' })}
+              value={endTime}
               onChange={(event, endTime) => this.setState({ endTime })}
               locale={locale}
             />
@@ -72,7 +107,16 @@ class CreateEvent extends React.Component {
               value={this.state.description}
               onChange={event => this.setState({ description: event.target.value })}
             />
-            <Button raised primary>{formatMessage({ id: 'send' })}</Button>
+
+            <Text>{ error }</Text>
+
+            <Button
+              raised
+              primary
+              onTouchTap={() => create(this.state)}
+            >
+              {formatMessage({ id: 'send' })}
+            </Button>
           </CardContent>
 
         </ResponsiveCard>
@@ -93,11 +137,28 @@ CreateEvent.propTypes = {
 export default injectIntl(connect(
   state => ({
     events: state.events.data,
+    error: state.events.error,
     locale: state.intl.locale,
   }),
   dispatch => ({
     refresh: () => {
       dispatch(rest.actions.events());
+    },
+    create: (event) => {
+      const timedEvent = event;
+
+      timedEvent.startDate.setHours(event.startTime.getHours());
+      timedEvent.startDate.setMinutes(event.startTime.getMinutes());
+
+      timedEvent.endDate.setHours(event.endTime.getHours());
+      timedEvent.endDate.setMinutes(event.endTime.getMinutes());
+
+      delete timedEvent.startTime;
+      delete timedEvent.endTime;
+
+      dispatch(rest.actions.events.post({}, {
+        body: JSON.stringify(timedEvent),
+      }));
     },
   }),
 )(CreateEvent));
